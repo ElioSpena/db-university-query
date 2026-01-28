4ea33c6d45f15de90d0c7d227bf3f8056c6bed57
ElioSpena <elio.spena.94@gmail.com>
2026-01-28 14:30:14 +0100

    feat: query con inner join

--- query.sql
+++ query.sql
@@ -16,22 +16,28 @@ SELECT *
 FROM `students`
 WHERE TIMESTAMPDIFF(YEAR,`date_of_birth`, CURDATE()) > 30;
 
+/* 3.5 (altro metodo) Selezionare tutti gli studenti che hanno più di 30 anni */
+SELECT *
+FROM `students`
+WHERE `date_of_birth` < DATE_SUB(CURDATE(), INTERVAL 30 YEAR)
+
+
 /* 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea(286) */
 SELECT *
 FROM `courses`
-WHERE `period` = "I semestre" 
+WHERE `period` = 'I semestre'
 AND `year` = 1
 
 /* 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21) */
 SELECT *
 FROM `exams`
-WHERE `date` = "2020/06/20"
-AND `hour` >= "14:00:00"
+WHERE `date` = '2020/06/20'
+AND `hour` >= '14:00:0'
 
 /* 6. Selezionare tutti i corsi di laurea magistrale (38) */
 SELECT *
 FROM `degrees`
-WHERE `level` = "magistrale"
+WHERE `level` = 'magistrale'
 
 /* 7. Da quanti dipartimenti è composta l'università? (12) */
 SELECT COUNT(`id`) AS `num_departments`
